61bc294215961773980d23b6083994fcf3293f6e
mrdevsam <mdshoykotislam05@gmail.com>
2023-10-20 16:13:27 +0600

    initial push

---
+++ src/main/java/com/example/spdataintro/SpdataintroApplication.java
@@ -0,0 +1,13 @@
+package com.example.spdataintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpdataintroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpdataintroApplication.class, args);
+	}
+
+}
