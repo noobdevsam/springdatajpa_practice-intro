8ee7ccb1ab814fc357ab29543b0158261951993b
mdshoykotislam05@gmail.com <mdshoykotislam05@gmail.com>
2023-10-21 10:55:59 +0600

    Initializing sample data

---
+++ src/main/java/com/example/spdataintro/bootstrap/DataInit.java
@@ -0,0 +1,26 @@
+package com.example.spdataintro.bootstrap;
+
+import org.springframework.stereotype.Component;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import com.example.spdataintro.repos.*;
+import com.example.spdataintro.model.*;
+import java.util.List;
+
+@Component
+public class DataInit {
+    
+    @Bean
+    CommandLineRunner runner(BookRepo bookRepo) {
+        var b1 = new Book("Domain Driven Design", "123", "RandomHouse");
+        var b2 = new Book("Spring In Action", "57567", "Oriely");
+        bookRepo.saveAll(List.of(b1,b2));
+        
+       bookRepo.findAll().forEach(
+           book -> {
+              System.out.println("Book Id: " + book.getId());
+              System.out.println("Book Title: " + book.getTitle());
+           }
+        );
+    }
+}
