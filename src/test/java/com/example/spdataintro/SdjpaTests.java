1a07ddffdffaf87750984da1d52b0762d2f86d11
mdshoykotislam05@gmail.com <mdshoykotislam05@gmail.com>
2023-10-25 00:44:58 +0600

    enabling MySQL compatibility in h2 db

--- src/test/java/com/example/spdataintro/SdjpaTests.java
+++ src/test/java/com/example/spdataintro/SdjpaTests.java
@@ -5,6 +5,7 @@ import com.example.spdataintro.repos.BookRepo;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.annotation.Commit;
 
@@ -13,6 +14,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @DataJpaTest
 @ComponentScan(basePackages = {"com.example.spdataintro.bootstrap"})
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class SdjpaTests {
     
     @Autowired
